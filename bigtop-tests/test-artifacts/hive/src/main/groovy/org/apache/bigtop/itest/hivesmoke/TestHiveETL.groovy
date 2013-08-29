e14fd218d35734435304282a70878412ba58d0d1
root <root@mrg40.lab.bos.redhat.com>
2013-08-29 14:51:33 -0400

    hive inside itest

---
+++ bigtop-tests/test-artifacts/hive/src/main/groovy/org/apache/bigtop/itest/hivesmoke/TestHiveETL.groovy
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.bigtop.itest.hivesmoke;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.apache.bigtop.itest.Contract;
+import org.apache.bigtop.itest.ParameterSetter;
+import org.apache.bigtop.itest.Property;
+import org.apache.bigtop.itest.shell.Shell;
+
+public class TestHiveETL {
+
+  public static Shell sh = new Shell("/bin/bash -s");
+
+  @BeforeClass
+  public static void setUp() throws ClassNotFoundException, InterruptedException, NoSuchFieldException, IllegalAccessException {
+    sh.exec("touch /tmp/hivetest");
+  }
+
+  @Test(timeout=120000L)
+  public void testCreate() throws SQLException {
+    sh.exec("shell_tests/test.sh");
+  }
+
+}
--- bigtop-tests/test-artifacts/hive/src/main/resources/shell_tests/test.sh
+++ bigtop-tests/test-artifacts/hive/src/main/resources/shell_tests/test.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
 # TODO: Eventually modify this to just run every shell script in the directory. So we dont explicitly have to specify each test and can just copy new tests into the directory.
+if [ -z $HIVE_HOME ];
+   then
+     echo "No HIVE_HOME set ! export it and try again"
+     exit 99;
+fi
 
 # Run HiveMix
-/root/ContinuousIntegration/SystemTests/Tests/hive/hive-crud.sh > hive-crud.out 2>&1
+source ./hive-crud.sh > hive-crud.out 2>&1
 
 grep -r DATABASE $TEST_DIR/hive-crud.out 
 
 echo "Hive Tests Have Completed $TEST_DIR"
 
-cat hive-crud.out | head
+cat hive-crud.out 
