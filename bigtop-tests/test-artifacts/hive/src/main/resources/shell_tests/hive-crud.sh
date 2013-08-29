eced4db9b190fc5351f3398a96876892f9e73a53
root <root@mrg40.lab.bos.redhat.com>
2013-08-29 00:59:41 -0400

    hide crud shell

---
+++ bigtop-tests/test-artifacts/hive/src/main/resources/shell_tests/hive-crud.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Setup environment variables
+
+TEST_DIR=/root/ContinuousIntegration/SystemTests/Test_Reports/hive
+rm -rf /tmp/root/hive.log
+# Clean up Before Test
+$HADOOP_HOME/bin/hadoop fs -rmr /tmp
+$HADOOP_HOME/bin/hadoop fs -rmr /user/hive/warehouse
+
+# Create the Directories for the Hive Data Warehouse
+$HADOOP_HOME/bin/hadoop fs -mkdir       /tmp
+$HADOOP_HOME/bin/hadoop fs -mkdir       /user/hive/warehouse
+$HADOOP_HOME/bin/hadoop fs -chmod g+w   /tmp
+$HADOOP_HOME/bin/hadoop fs -chmod g+w   /user/hive/warehouse
+
+# Run Hive CRUD Test
+echo "Running Hive CRUD Test"
+echo "HIVE HOME  : $HIVE_HOME"
+cd $HIVE_HOME
+$HIVE_HOME/bin/hive --verbose -f /root/ContinuousIntegration/SystemTests/Tests/hive/hive-crud.q
+echo "Hive CRUD Test Completed"
+echo ""
+
+#Grep the exceptions
+grep -A 2 -B 2 -r Exception /tmp/root/hive.log
+
+# Condition to determine Test Case Success
+if cat $TEST_DIR/hive-crud.out | grep -q 'Time taken:'; then echo "TEST SUCCEEDED" ; else echo "TEST FAILED"; fi
+
+
+
